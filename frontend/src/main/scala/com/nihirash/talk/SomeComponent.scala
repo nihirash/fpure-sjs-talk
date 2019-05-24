3d0bf79528bc31e128982eae617b156b2fa1cf5f
nihirash <anihirash@gmail.com>
2019-05-24 12:43:05 +0300

    clean up

--- frontend/src/main/scala/com/nihirash/talk/SomeComponent.scala
+++ frontend/src/main/scala/com/nihirash/talk/SomeComponent.scala
@@ -4,13 +4,9 @@ import com.nihirash.talk.models.TodoItem
 import slinky.core.StatelessComponent
 import slinky.core.annotations.react
 import slinky.core.facade.ReactElement
-import slinky.web.html.{className, div}
 
 @react class SomeComponent extends StatelessComponent{
   case class Props(element: TodoItem)
 
-  def render(): ReactElement = {
-
-    div(className := styles.someClass)(props.element.toString)
-  }
+  def render(): ReactElement = "hello"
 }
